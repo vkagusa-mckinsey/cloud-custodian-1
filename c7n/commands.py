Sutto/cloud-custodian

Honeycomb tracing support, v1

c7n/commands.py
@@ -24,6 +24,8 @@ import pprint
 import sys
 import time
 
+import beeline
+
 import six
 import yaml
 from yaml.constructor import ConstructorError
@@ -38,6 +40,7 @@ from c7n.credentials import UnableToAssumeRole
 from c7n import provider
 from c7n.resources import load_resources
 
+from c7n.beeline_tracer import traceable
 from c7n import error_tracking
 log = logging.getLogger('custodian.commands')
 
@@ -270,6 +273,7 @@ def validate(options):
 AWS_ERROR_BLACKLIST = ['OptInRequired', 'SubscriptionRequiredException', 'EndpointConnectionError']
 
 @policy_command
+@traceable(name='c7n-run')
 def run(options, policies):
     exit_code = 0
 
@@ -283,44 +287,46 @@ def run(options, policies):
             sys.exit(1)
 
     for policy in policies:
-        try:
-            policy()
-        except UnableToAssumeRole:
-            log.warning("Unable to assume the especified role.")
-        except EndpointConnectionError:
-            log.warning("Unable to find the correct endpoint - this usually means a new server.")
-        except ClientError as e:
-            # AWS Client error, we can handle + resume this, in theory - but we should log
-            # it as a warning to the error tracker with some context.
-            error_body = e.response['Error']
-            error_code = error_body.get('Code', 'Unknown')
-            if error_code in ['AccessDenied', 'AccessDeniedException', 'UnauthorizedOperation', 'AuthorizationError']:
-                log.warning("Reported access denied exception in %s, continuing: %s" % (policy.name, repr({
-                    'error_details': error_body,
-                    'policy': repr(policy),
-                    'policy_data': policy.data,
-                    'policy_options': policy.options,
-                    'expected_permissions': list(policy.get_permissions()),
-                })))
-            elif error_code in AWS_ERROR_BLACKLIST:
-                # DO nothing... We don't report these.
-                log.warning("Got error on blacklist: %s" % (error_code,))
-            else:
-                # Handle it here, but we skip.
+        with beeline.tracer(name="outer-policy"):
+            try:
+                policy()
+            except UnableToAssumeRole:
+                log.warning("Unable to assume the especified role.")
+            except EndpointConnectionError:
+                log.warning("Unable to find the correct endpoint - this usually means a new server.")
+            except ClientError as e:
+                # AWS Client error, we can handle + resume this, in theory - but we should log
+                # it as a warning to the error tracker with some context.
+                error_body = e.response['Error']
+                error_code = error_body.get('Code', 'Unknown')
+                if error_code in ['AccessDenied', 'AccessDeniedException', 'UnauthorizedOperation', 'AuthorizationError']:
+                    log.warning("Reported access denied exception in %s, continuing: %s" % (policy.name, repr({
+                        'error_details': error_body,
+                        'policy': repr(policy),
+                        'policy_data': policy.data,
+                        'policy_options': policy.options,
+                        'expected_permissions': list(policy.get_permissions()),
+                    })))
+                elif error_code in AWS_ERROR_BLACKLIST:
+                    # DO nothing... We don't report these.
+                    log.warning("Got error on blacklist: %s" % (error_code,))
+                else:
+                    # Handle it here, but we skip.
+                    error_tracking.report_exception()
+                    log.warning("Reported generic client exception in %s as %s, continuing." % (policy.name, error_code))
+            except Exception:
+                exit_code = 2
                 error_tracking.report_exception()
-                log.warning("Reported generic client exception in %s as %s, continuing." % (policy.name, error_code))
-        except Exception:
-            exit_code = 2
-            error_tracking.report_exception()
-            if options.debug:
-                raise
-            log.exception(
-                "Error in policy: %s" % (
-                    policy.name))
+                if options.debug:
+                    raise
+                log.exception(
+                    "Error in policy: %s" % (
+                        policy.name))
     if exit_code == 0:
         log.info("Successfully ran %d policies." % len(policies))
     else:
         log.info("Ran %d policies, encountered an unhandled exception." % len(policies))
+        beeline.close()
         sys.exit(exit_code)
 
 

vkagusa-mckinsey/cloud-custodian-1

c7n/commands.py
@@ -10,7 +10,7 @@ import itertools
 import logging
 import os
 import sys
-
+import beeline
 import yaml
 from yaml.constructor import ConstructorError
 
@@ -25,6 +25,7 @@ from c7n import provider
 from c7n.resources import (
     load_resources, load_available, load_providers, PROVIDER_NAMES)
 
+from c7n.beeline_tracing import traceable
 from c7n import error_tracking
 log = logging.getLogger('custodian.commands')
 
@@ -259,6 +260,7 @@ def validate(options):
 AWS_ERROR_BLACKLIST = ['OptInRequired', 'SubscriptionRequiredException', 'EndpointConnectionError']
 
 @policy_command
+@traceable(name='c7n-run')
 def run(options, policies):
     exit_code = 0
 
@@ -273,30 +275,35 @@ def run(options, policies):
 
     errored_policies = []
     for policy in policies:
-        try:
-            policy()
-        except UnableToAssumeRole:
-            log.warning("Unable to assume the especified role.")
-        except EndpointConnectionError:
-            log.warning("Unable to find the correct endpoint - this usually means a new server.")
-        except ClientError as e:
-            # AWS Client error, we can handle + resume this, in theory - but we should log
-            # it as a warning to the error tracker with some context.
-            error_body = e.response['Error']
-            error_code = error_body.get('Code', 'Unknown')
-            if error_code in ['AccessDenied', 'AccessDeniedException', 'UnauthorizedOperation', 'AuthorizationError']:
-                log.warning("Reported access denied exception in %s, continuing: %s" % (policy.name, repr({
-                    'error_details': error_body,
-                    'policy': repr(policy),
-                    'policy_data': policy.data,
-                    'policy_options': policy.options,
-                    'expected_permissions': list(policy.get_permissions()),
-                })))
-            elif error_code in AWS_ERROR_BLACKLIST:
-                # DO nothing... We don't report these.
-                log.warning("Got error on blacklist: %s" % (error_code,))
-            else:
-                # Handle it here, but we skip.
+        with beeline.tracer(name="outer-policy"):
+            try:
+                policy()
+            except UnableToAssumeRole:
+                log.warning("Unable to assume the especified role.")
+            except EndpointConnectionError:
+                log.warning("Unable to find the correct endpoint - this usually means a new server.")
+            except ClientError as e:
+                # AWS Client error, we can handle + resume this, in theory - but we should log
+                # it as a warning to the error tracker with some context.
+                error_body = e.response['Error']
+                error_code = error_body.get('Code', 'Unknown')
+                if error_code in ['AccessDenied', 'AccessDeniedException', 'UnauthorizedOperation', 'AuthorizationError']:
+                    log.warning("Reported access denied exception in %s, continuing: %s" % (policy.name, repr({
+                        'error_details': error_body,
+                        'policy': repr(policy),
+                        'policy_data': policy.data,
+                        'policy_options': policy.options,
+                        'expected_permissions': list(policy.get_permissions()),
+                    })))
+                elif error_code in AWS_ERROR_BLACKLIST:
+                    # DO nothing... We don't report these.
+                    log.warning("Got error on blacklist: %s" % (error_code,))
+                else:
+                    # Handle it here, but we skip.
+                    error_tracking.report_exception()
+                    log.warning("Reported generic client exception in %s as %s, continuing." % (policy.name, error_code))
+            except Exception:
+                exit_code = 2
                 error_tracking.report_exception()
                 log.warning("Reported generic client exception in %s as %s, continuing." % (policy.name, error_code))
         except Exception:
@@ -314,6 +321,7 @@ def run(options, policies):
         log.info("Ran %d policies, encountered an unhandled exception." % len(policies))
         log.error("The following policies had errors while executing\n - %s" % (
             "\n - ".join(errored_policies)))
+        beeline.close()
         sys.exit(exit_code)
 
 